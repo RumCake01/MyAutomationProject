18dec90c4456e5b41770874fa679bdb62ecf660f
RumCake <mia.hasnutdinova@gmail.com>
2020-12-24 00:39:59 -0500

    added json report to the TestRunner class

--- src/test/java/com/myapp/runners/TestRunner.java
+++ src/test/java/com/myapp/runners/TestRunner.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"json:target/cucumber.json"},
         features = "src/test/resources/features",
         glue = "com/myapp/step_definitions",
         dryRun = true
